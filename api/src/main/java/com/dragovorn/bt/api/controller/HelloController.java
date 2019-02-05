d3dd9f3384d2e8495043b2b8381810bd118f3c0b
Andrew Burr <me@dragovorn.com>
2019-02-05 13:55:35 -0500

    Commit initial springboot test code

--- api/src/main/java/com/dragovorn/bt/api/controller/HelloController.java
+++ api/src/main/java/com/dragovorn/bt/api/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.dragovorn.bt.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Hello, World!";
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
 }
