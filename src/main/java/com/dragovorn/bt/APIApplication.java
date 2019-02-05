1bf46056688f93a6a5a011a1b4fa5de8069cd8d1
Andrew Burr <me@dragovorn.com>
2019-02-05 14:02:56 -0500

    Finish up my refactor

--- src/main/java/com/dragovorn/bt/APIApplication.java
+++ src/main/java/com/dragovorn/bt/APIApplication.java
@@ -1,4 +1,4 @@
-package com.dragovorn.bt.api;
+package com.dragovorn.bt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/dragovorn/bt/controller/HelloController.java
+++ src/main/java/com/dragovorn/bt/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.dragovorn.bt.api.controller;
+package com.dragovorn.bt.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
